e92ebeae00d25fde904e9ae6156424af775882fe
felipecruz17 <feemattioli@gmail.com>
2022-10-27 23:23:56 -0300

    Config inicial - Criação dos componentes

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus ab temporibus quidem, possimus, inventore repudiandae ipsum repellat impedit est incidunt quis. Error molestiae aspernatur, ut porro ipsa laborum deserunt ex?',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
